4d9596765cbed0b05d9a0b9a47291d6d9528033a
workdjoshi-afk <work.djoshi@gmail.com>
2026-05-02 18:06:19 +0530

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.commpansers.pathshala',
+  appName: 'Commanders Pathshala',
+  webDir: 'dist'
+};
+
+export default config;
